1624db58ef9b80d8dac533ba446af8f42e2bf85b
Jose Sanchis <sanchisbeldajose@gmail.com>
2019-09-23 17:40:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//Import components
+import Header from './Components/Header/Header.jsx';
+import Body from './Components/Body/Body.jsx';
+import Footer from './Components/Footer/Footer.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Header />
+      <Body />
+      <Footer />
+    </main>
   );
 }
 
